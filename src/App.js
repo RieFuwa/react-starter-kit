99f09890fdd371953aa2766b2e462d3c9566aa96
RieFuwa <bedirhankabatas1@gmail.com>
2022-12-05 15:11:03 +0300

    '05.12.2022'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState, useEffect } from 'react';
+
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [mail, setMail] = useState("")
+  const [artir, setArtir] = useState(0)
+
+  useEffect(()=>{
+    
+  })
+
+  const handleSumbit = () => {
+    console.log(title)
+    console.log(mail)
+  }
+
+  const handleTitle = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleMail = (event) => {
+    setMail(event.target.value)
+  }
+
+
+  const sayiArtir = () => {
+    setArtir(artir + 1)
+  }
+  const sayiAzalt = () => {
+    setArtir(artir - 1)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSumbit}>
+        <div>
+          <label>Username:</label>
+          <input type='text' value={title} onChange={handleTitle}></input>
+          <label>Mail:</label>
+          <input type='text' value={mail} onChange={handleMail}></input>
+        </div>
+    
+        <button type='sumbit'>send</button>
+      </form>
+
+
+
+      <div>{artir}</div>
+      <button onClick={sayiArtir}>Artir</button>
+      <button onClick={sayiAzalt}>Azalt</button>
     </div>
   );
 }
